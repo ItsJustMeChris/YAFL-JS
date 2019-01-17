87298f00eeef83ebc316640a32ac460aa284d282
Chris <dribbleribbletribble@gmail.com>
2019-01-17 13:38:49 -0500

    Update
    Made this a bit more 'function-programming' oriented.

--- yafl.js
+++ yafl.js
@@ -1,18 +1,18 @@
 (function () {
-    let forms = Array.from(document.getElementsByClassName('yafl'));
+    let forms = Array.from(document.getElementsByClassName('yafl')).map((form) => {
+        form.steps = Array.from(form.getElementsByClassName('yafl-step'));
+        form.buttons = Array.from(form.getElementsByClassName('yafl-button'));
+        form.progress = 0;
+        form.conditionals = Array.from(form.getElementsByClassName('yafl-conditional')).map((conditional) => {
+            conditional.targets = Array.from(document.getElementsByName(conditional.dataset.target));
+            return conditional;
+        });
+        return form;
+    });
 
-    let formSteps = forms.map(e => Array.from(e.getElementsByClassName('yafl-step')));
-
-    let formButtons = forms.map(e => Array.from(e.getElementsByClassName('yafl-button')));
-
-    let formProgress = forms.map(e => 0);
-
-    let formConditionals = Array.from(document.getElementsByClassName('yafl-conditional'));
-
-    function setupConditionals() {
-        formConditionals.forEach((c) => {
-            let conditionalTargets = document.getElementsByName(c.dataset.target);
-            conditionalTargets.forEach((t) => {
+    forms.forEach((form) => {
+        form.conditionals.forEach((c) => {
+            c.targets.forEach((t) => {
                 t.addEventListener('input', (e) => {
                     if (c.dataset.regex != undefined) {
                         let reg = new RegExp(c.dataset.regex);
@@ -30,7 +30,27 @@
                 });
             });
         });
-    }
+
+        form.buttons.forEach((button) => {
+            button.addEventListener('click', (e) => {
+                if (form.progress != form.steps.length - 1) {
+                    e.preventDefault();
+                }
+                form.progress += 1;
+                if (form.progress == form.steps.length - 1) {
+                    button.innerHTML = "Submit";
+                }
+                for (let i = 0; i < form.steps.length; i++) {
+                    let step = form.steps[i];
+                    if (i != form.progress) {
+                        step.style.display = "none";
+                    } else {
+                        step.style.display = "";
+                    }
+                }
+            });
+        });
+    });
 
     function showHide(e, bool) {
         if (bool) {
@@ -47,39 +67,4 @@
             }
         }
     }
-
-    function setupForms() {
-        for (let i = 0; i < forms.length; i++) {
-            for (let b = 0; b < formButtons[i].length; b++) {
-                formButtons[i][b].addEventListener('click', (e) => {
-                    if (formProgress[i] != formSteps[i].length - 1) {
-                        e.preventDefault();
-                    }
-                    formProgress[i] += 1;
-                    if (formProgress[i] == formSteps[i].length - 1) {
-                        formButtons[i][b].innerHTML = "Submit";
-                    }
-                    showNextStep();
-                });
-            }
-        }
-    }
-
-    function showNextStep() {
-        for (let f = 0; f < forms.length; f++) {
-            let progress = formProgress[f];
-            for (let i = 0; i < formSteps[f].length; i++) {
-                let step = formSteps[f][i];
-                if (i != progress) {
-                    step.style.display = "none";
-                } else {
-                    step.style.display = "";
-                }
-            }
-        }
-    }
-
-    showNextStep();
-    setupForms();
-    setupConditionals();
 })();
